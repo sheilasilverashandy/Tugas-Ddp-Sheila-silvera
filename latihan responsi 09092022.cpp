fce064754ef62ca321183e0563b900444a705c1e
sheilasilverashandy <112568485+sheilasilverashandy@users.noreply.github.com>
2022-09-12 11:46:07 +0700

    Add files via upload

---
+++ latihan responsi 09092022.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+    {
+    	float luas, phi=3.14;
+    	int r;
+    	cout<<"masukan jari-jari : ";
+    	cin>>r;
+    	
+    	luas=phi*r*r;
+    	
+    	cout<<"luas lingkaran = "<<luas<<endl;
+    	return 0;
+	}
